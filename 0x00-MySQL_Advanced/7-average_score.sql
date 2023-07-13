198563947ac11274b2ecc555c3488ca1fc0ae250
josaya <josaya@josaya.josaya>
2023-07-14 00:44:40 +0200

    committing average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,8 +1,12 @@
 --Desc: creates a stored procedure that computesz and stores the average score for a student
 
+DELIMITER $$
+
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE average_score FLOAT;
     SELECT AVG(score) INTO average_score FROM corrections WHERE user_id = user_id;
     INSERT INTO average_scores (user_id, average_score) VALUES (user_id, average_score);
 END$$
+
+DELIMITER ;
