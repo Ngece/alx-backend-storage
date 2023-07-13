373cbf9bb532e76737bd4c08a71bd80c7e6ea26b
josaya <josaya@josaya.josaya>
2023-07-14 00:22:16 +0200

    committing valid email

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,14 +1,11 @@
 --Desc: creates a trigger that resets the attribute valid_email whenever the email is updated
 
-DELIMITER //
-
-CREATE TRIGGER update_valid_email
+DELIMITER $$
+CREATE TRIGGER valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = 0;
-    END IF;
-END //
+    SET NEW.valid_email = 0;
+END$$
 
 DELIMITER ;
