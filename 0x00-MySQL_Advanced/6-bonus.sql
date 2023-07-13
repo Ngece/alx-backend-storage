0dbef4cb0965df97f62c278e1bf27f9dc650effc
josaya <josaya@josaya.josaya>
2023-07-14 00:28:15 +0200

    committing bonus

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,10 @@
+--Desc: cfreates a stored procedure that adds a new correction for student
+
+DELIMITER $$
+CREATE PROCEDURE add_bonus(IN user_id INT, IN project_name VARCHAR(255), )IN score FLOAT)
+BEGIN
+    INSERT INTO corrections (user_id, project_name, score)
+    VALUES (user_id, project_name, score);
+END$$
+
+DELIMITER ;
