5fd16accdc0029689a476e821e4f8417356f531b
josaya <josaya@josaya.josaya>
2023-07-14 01:36:59 +0200

    committing index my name

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,3 @@
---Desc: creaaes an index idx_name_first on the tables names and the first letter of the name.
+-- Desc: creaaes an index idx_name_first on the tables names and the first letter of the name.
+
 CREATE INDEX idx_name_first ON names ( name(1) );
