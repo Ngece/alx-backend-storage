3aebfec98b7c15889472b65c538fcbf7b9168fc1
josaya <josaya@josaya.josaya>
2023-07-14 01:30:55 +0200

    committing div

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,7 +1,7 @@
 -- Desc: creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
 
 DELIMITER $$
-CREATE FUNCTION SafeDiv(IN a INT, IN b INT)
+CREATE FUNCTION SafeDiv(IN a FLOAT, IN b FLOAT)
 RETURNS FLOAT
 BEGIN
     IF a = 0 THEN
