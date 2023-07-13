128c54064f920d8fb53a9e8255d06b22e77a8ae6
josaya <josaya@josaya.josaya>
2023-07-13 23:34:35 +0200

    committing uniq ussers

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
--- Desc: Create a table of users with unique email addresses, id's, and names.
+-- Desc: Creates a table of users with unique email addresses, id's, and names.
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255)
 );
