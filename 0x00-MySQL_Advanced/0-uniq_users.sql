3049a3736480407abbbc08056f08851820b5d3d5
josaya <josaya@josaya.josaya>
2023-07-13 23:19:46 +0200

    committing uniq users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Desc: Create a table of users with unique email addresses, id's, and names.
-CREATE TABLE If NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
