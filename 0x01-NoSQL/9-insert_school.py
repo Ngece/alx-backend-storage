3a80d375134683ed97f9944465efa4f82ea35418
josaya <josaya@josaya.josaya>
2023-07-19 00:12:31 +0200

    committing insert school

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,4 +1,4 @@
-#!//usr/bin/env python3
+#!/usr/bin/env python3
 """Contains function that inserts a new document in a collection based on kwargs"""
 
 def insert_school(mongo_collection, **kwargs):
