84d862197517851cb96d9d2e52efe7b8f5027d06
josaya <josaya@josaya.josaya>
2023-07-19 00:42:36 +0200

    committing log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -14,11 +14,13 @@ total_logs = collection.count_documents({})
 print(f"{total_logs} logs")
 
 methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+print("Methods:")
 for method in methods:
     count = collection.count_documents({"method": method})
-    print(f"method {method}: {count}")
+    print(f"\tmethod {method}: {count}")
 
 status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
 print(f"{status_check_count} status check")
 
 client.close()
+
