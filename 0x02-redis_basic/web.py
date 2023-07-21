2ed8d8c8fcccc871f02c25eaca29588b6d073db0
Ngece <ngecemlamli@gmail.com>
2023-07-21 22:41:38 +0200

    committing web.py again

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,18 +6,17 @@ from functools import wraps
 import requests
 from typing import Callable
 
-
 cache = redis.Redis()
 
 
 def count_calls(method: Callable) -> Callable:
-    """Decorator to count number of times functions are called"""
+    """Decorator to count the number of times functions are called"""
     @wraps(method)
     def count_calls_wrapper(*args) -> str:
-        """Counts number of calls wrapped function makes"""
+        """Counts the number of calls the wrapped function makes"""
         key = f"count:{args[0]}"
         cache.incr(key)
-        cache.setex('count', 10, cache.get(key))
+        cache.expire(key, 10)
         return method(*args)
 
     return count_calls_wrapper
@@ -27,5 +26,11 @@ def count_calls(method: Callable) -> Callable:
 def get_page(url: str) -> str:
     """GET HTML content from URL and return it"""
     r = requests.get(url)
-
     return r.text
+
+
+# Test the function with a slow response URL
+if __name__ == "__main__":
+    slow_url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"
+    html_content = get_page(slow_url)
+    print(html_content)
